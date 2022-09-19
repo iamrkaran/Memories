634fc4b282d2edfb9fbb89f01cd2ecf3e2f35a65
iamClutchX <iamr.karan@gmail.com>
2022-09-19 16:27:43 +0530

    Revert "make it better"
    This reverts commit 6acb8f30486f6decf172b0618a356aaf3edbe1a1.

--- server/index.js
+++ server/index.js
@@ -15,9 +15,6 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.get('/',(req,res)=>{
-    res.send('App is Running');
-});
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
