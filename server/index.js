0530b6f3e7d2730ec9d05f9523dfc051a13c28d5
iamClutchX <iamr.karan@gmail.com>
2022-09-19 16:29:38 +0530

    final

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+app.get('/', (req, res) => {  
+  res.send('Hello Running');
+});
+
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
